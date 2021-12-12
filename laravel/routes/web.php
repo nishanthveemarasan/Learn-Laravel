75ebb16e49b46449eb47f3d92783577ed2974161
nishanthveema <akveemarajan@gmail.com>
2021-12-12 13:20:04 +0000

    Request and Response

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -47,15 +47,80 @@ Route::name('users')->group(function () {
     })->name('get');
 });
 //create a post
-Route::prefix('post')->group(function () {
+Route::prefix('post')->name('posts.')->group(function () {
     Route::get('/create', function () {
         $proposal = new Proposal();
         $proposal->title = 'second title';
         $proposal->content = 'come content';
         $proposal->save();
         return $proposal;
-    });
+    })->name('get');
     Route::get('/get/{uuid}', function (Proposal $proposal) {
         return $proposal;
-    });
+    })->name('create');
 });
+
+/**
+ * Request and Response
+ */
+Route::get('/fun/responses/{proposal:uuid}', function (Proposal $proposal) {
+    return response($proposal, 201)->withHeaders(['Content-type' => 'application/json'])->cookie('MY_COOKIE', 'NIshanth', 3600);
+});
+
+Route::get('/fun/json/{proposal:uuid}', function (Proposal $proposal) {
+    return response()->json([
+        'name' => $proposal->toArray(),
+    ]);
+});
+
+Route::get('/fun/download/{proposal:uuid}', function (Proposal $proposal) {
+    //(pathName , name of the file , headers)
+    return response()->download(public_path('/danial.jpg'), 'face.jpg', []);
+});
+
+//Request
+Route::get('/send-request', function () {
+    return redirect()->route('request.get', ['id' => 1, 'photos' => 'yes', 'table' => 'no']);
+});
+Route::get('access-request', function (Request $request) {
+    //access all requests
+    //dd($request->all());
+    $all = $request->all();
+    ///access a specific request
+    $photos = $request->input('photos');
+    // dd($photos);
+    //access a request header
+    $header = $request->header('X-header-Name'); //null if not
+    //access bearerToken
+    $token = $request->bearerToken();
+    //pass default value to a input
+    $input = $request->input('name', 'Nishanth');
+    //dd($input);
+    //access an input from an array
+    $arrayInput = $request->input('array.0.name');
+    //access query string
+    $element = $request->query('id');
+    //access JSON input values
+    $jsonValue = $request->input('user.name');
+    //Using onl A portion of the input data
+    $input = $request->only(['id', 'photos']);
+    $input = $request->except(['table']);
+    //Determining If Input Is Present
+    /*      //has(['val1','val2'])
+            if ($request->has('id')) {
+                return 'is there';
+            }
+       */
+    /*
+      The whenHas method will execute the given closure if a value is present on the request:
+    $request->whenHas('id', function ($input) {
+        return 'hi';
+    });
+    */
+    //determine if a value is present and not empty
+    $check = $request->filled('id');
+
+    //Retriving Cookies from Requests
+
+    $value = $request->cookie('name');
+})->name('request.get');
